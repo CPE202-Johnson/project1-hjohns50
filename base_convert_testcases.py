12df2a2f6dc871c3d800ddb9ba4693b64e16f0a6
Henry Johnson <henryjohnson@Henrys-MacBook-Pro-3.local>
2021-01-18 21:58:12 -0800

    third try

--- base_convert_testcases.py
+++ base_convert_testcases.py
@@ -11,6 +11,9 @@ class TestBaseConvert(unittest.TestCase):
 
     def test_base16(self):
         self.assertEqual(convert(316,16),"13C")
+    
+    def test_base15(self):
+        self.assertEqual(convert(11259375,16),"ABCDEF")
 
 if __name__ == "__main__":
         unittest.main()
